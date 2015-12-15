3cfec5ad09d6be9ea29e8291be08393b48228fb2
Varya Stepanova <mail@varya.me>
2015-12-15 14:23:51 +0200

    Clear structure with pages and modules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-import MainApp from './modules/main/main.app.js'
+import angular from 'angular';
+import ngRoute from 'angular-route';
+import react from 'ngreact';
 
-MainApp();
+import getGlobal from './_helpers/getGlobal.js'
+
+import MainController from './pages/index/index.ctrl.js';
+import Menu from './modules/menu/menu.component.js'
+
+let App = () => {
+
+    var myApp = angular.module("myApp", [
+    "ngRoute", // TODO: try ui.router
+    "react"
+    ]);
+    getGlobal().myApp = myApp;
+    myApp.config(($routeProvider, $locationProvider) => {
+        $routeProvider
+            .when('/pages/:pageId', {
+                template: require("./pages/index/index.html"),
+                controller: MainController
+            })
+            .when('/', {
+                redirectTo: '/pages/index'
+            })
+
+        $locationProvider.html5Mode(true);
+    })
+    myApp.value('Menu', Menu); // Todo: move somewhere
+}
+
+App();
