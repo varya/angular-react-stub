bc6deac8debcc9707c930d1fdba92fd56edda73b
Varya Stepanova <mail@varya.me>
2015-12-15 15:58:46 +0200

    Clean JavaScript code

--- src/_helpers/getGlobal.js
+++ src/_helpers/getGlobal.js
@@ -1,12 +1,14 @@
 export default function() {
-    // Workers don’t have `window`, only `self`
-    if (typeof self !== 'undefined') {
-        return self;
-    }
-    if (typeof global !== 'undefined') {
-        return global;
-    }
-    // Not all environments allow eval and Function
-    // Use only as a last resort:
-    return new Function('return this')();
- }
+  // Workers don’t have `window`, only `self`
+  if (typeof self !== 'undefined') {
+    return self;
+  }
+
+  if (typeof global !== 'undefined') {
+    return global;
+  }
+
+  // Not all environments allow eval and Function
+  // Use only as a last resort:
+  return new Function('return this')();
+}
--- src/index.js
+++ src/index.js
@@ -5,28 +5,29 @@ import react from 'ngreact';
 // controllers
 import MainController from './pages/index/index.ctrl.js';
 
-let App = () => {
+var App = () => {
 
-    return angular.module("myApp", [
-    "ngRoute", // TODO: try ui.router
-    "react"
+  return angular.module('myApp', [
+    'ngRoute', // TODO: try ui.router
+    'react',
     ]).config(($routeProvider, $locationProvider) => {
-        $routeProvider
-            .when('/pages/:pageId', {
-                template: require("./pages/index/index.html"),
-                controller: MainController
-            })
-            .when('/', {
-                redirectTo: '/pages/index'
-            })
+      $routeProvider
+        .when('/pages/:pageId', {
+          template: require('./pages/index/index.html'),
+          controller: MainController,
+        })
+        .when('/', {
+          redirectTo: '/pages/index',
+        });
 
-        $locationProvider.html5Mode(true);
+      $locationProvider.html5Mode(true);
+
     });
 
-}
+};
 
-let myApp = App();
+var myApp = App();
 
 // Components to use
-import Menu from './modules/menu/menu.component.js'
+import Menu from './modules/menu/menu.component.js';
 myApp.value('Menu', Menu); // Todo: move somewhere
